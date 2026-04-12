88d707138533b3862e7cb194920089b6451801f9
JacobLynch <jacob.lynch@blockworks.co>
2026-04-12 00:57:19 -0700

    Add ProjectileSpawnRouter for shared fire-input dispatch

---
+++ godot/simulation/systems/projectile_spawn_router.gd
@@ -0,0 +1,42 @@
+class_name ProjectileSpawnRouter
+
+static func handle_fire(
+		player: PlayerEntity,
+		input: Dictionary,
+		projectile_system: ProjectileSystem,
+		context: Dictionary) -> void:
+
+	var flags: int = input.get("action_flags", 0)
+	if (flags & MessageTypes.InputActionFlags.FIRE) == 0:
+		return
+	if not projectile_system.can_fire(player.player_id):
+		return
+
+	var aim: Vector2 = player.aim_direction
+	var type_id: int = ProjectileType.Id.TEST
+	var params: ProjectileParams = ProjectileType.get_params(type_id)
+
+	if context.get("authoritative", false):
+		var rtt_ms: int = context["rtt_ms"]
+		var history: PlayerPositionHistory = context["position_history"]
+		var tick: int = context["tick"]
+		var rewind_ticks: int = int(round((rtt_ms / 2.0) / MessageTypes.TICK_INTERVAL_MS))
+		var rewound_pos: Vector2 = history.lookup(player.player_id, tick - rewind_ticks)
+		var origin: Vector2 = rewound_pos + aim * params.spawn_offset
+		var proj: ProjectileEntity = projectile_system.spawn_authoritative(
+			player.player_id, type_id, origin, aim, input["input_seq"])
+		proj.advance(rtt_ms / 2000.0, projectile_system.get_walls(), [], [])
+		context["spawn_events"].append({
+			"projectile_id": proj.projectile_id,
+			"type_id": type_id,
+			"owner_player_id": player.player_id,
+			"origin": proj.position,   # server-now, post fast-forward
+			"direction": aim,
+			"input_seq": input["input_seq"],
+		})
+	else:
+		var origin := player.position + aim * params.spawn_offset
+		projectile_system.spawn_predicted(
+			player.player_id, type_id, origin, aim, input["input_seq"])
+
+	projectile_system.start_cooldown(player.player_id)
