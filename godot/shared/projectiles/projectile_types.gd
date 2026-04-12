c4228b49d1e9cc1aff087308e04bb039aeb71929
JacobLynch <jacob.lynch@blockworks.co>
2026-04-11 23:46:06 -0700

    ProjectileType.get_params: push_error on unknown type_id

--- godot/shared/projectiles/projectile_types.gd
+++ godot/shared/projectiles/projectile_types.gd
@@ -8,4 +8,5 @@ static func get_params(type_id: int) -> ProjectileParams:
 	match type_id:
 		Id.TEST:
 			return preload("res://shared/projectiles/test_projectile.tres")
+	push_error("ProjectileType.get_params: unknown type_id %d" % type_id)
 	return null
